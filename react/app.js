403e5f733744de9ed93fb88b95e3bd81b6fc9892
Lee Copland <lee@leecopland.com>
2017-11-06 16:01:20 +1100

    Add elements to app.js

--- react/app.js
+++ react/app.js
@@ -1,4 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import HelloWorld from './components/HelloWorld'
+import StreamingScreen from './components/StreamingScreen'
+import ChatSection from './components/ChatSection'
+import QuestionSection from './components/QuestionSection'
+import TextInput from './components/TextInput'
+import SwitchButton from './components/SwitchButton'
 ReactDOM.render(<HelloWorld />, document.getElementById('app'))
