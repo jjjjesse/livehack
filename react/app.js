5db2b1fd521bf3d50e510153ea8723905afef016
Jesse Tilley <jesseandrewtilley@gmail.com>
2017-11-06 22:21:22 +1100

    Purge of unholy code

--- react/app.js
+++ react/app.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-// import {Main, LeftColumn, RightColumn} from './components/Main'
 import StreamingScreen from './components/StreamingScreen'
 
 ReactDOM.render(<StreamingScreen />, document.getElementById('app'))
