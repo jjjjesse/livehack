9fb476d62d4762206d14faf07237b8fe86993787
Richard Denton <isdampe@gmail.com>
2017-11-06 16:27:23 +1100

    PROGRESS: Add basic channels to app.js (its messy\!)

--- server/tests/client-ws.js
+++ server/tests/client-ws.js
@@ -1,11 +1,20 @@
 const WebSocket = require('ws');
 
-const ws = new WebSocket('ws://localhost:8080');
+const ws = new WebSocket('ws://localhost:8080/hacks');
 ws.on('open', function open() {
+
+	ws.send(JSON.stringify({
+  	'type': 'register',
+    'name': 'Richard',
+    'message': ''
+  }));
+
   ws.send(JSON.stringify({
+  	'type': 'broadcast',
     'name': 'Richard',
     'message': 'This is a test.'
   }));
+
 });
 
 ws.on('message', function incoming(data) {
