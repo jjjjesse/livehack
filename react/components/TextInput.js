d7ed5dc05090cc739daac21a71d59d9fee6fe3fc
Lee Copland <lee@leecopland.com>
2017-11-07 16:43:48 +1100

    text input form type

--- react/components/TextInput.js
+++ react/components/TextInput.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import * as ReactBootstrap from 'react-bootstrap'
+import './TextInput.css'
 
 const TextInput = () => {
   return (
       <form>
-        <ReactBootstrap.FormGroup controlId="formBasicText" >
-          <ReactBootstrap.FormControl type="text" placeholder="Enter text"/>
+        <ReactBootstrap.FormGroup controlId="formControlsTextarea">
+          <ReactBootstrap.ControlLabel>Chat</ReactBootstrap.ControlLabel>
+            <ReactBootstrap.FormControl componentClass="textarea" placeholder="Enter Text" />
         </ReactBootstrap.FormGroup>
       </form> )}
 
