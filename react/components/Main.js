4130afdb8d0d79a83ab4677ee31e9e51c48c7706
Jesse Tilley <jesseandrewtilley@gmail.com>
2017-11-08 14:13:00 +1100

    Add toggle to turn off Stream

--- react/components/Main.js
+++ react/components/Main.js
@@ -1,16 +1,14 @@
 import React from 'react'
-import { Grid, Row, Col } from 'react-bootstrap'
+import { Grid, Row, Col, Button } from 'react-bootstrap'
 import StreamingScreen from './StreamingScreen'
 import ChatSection from './ChatSection'
 import QuestionSection from './QuestionSection'
 import TextInput from './TextInput'
 import Login from './Login'
 
-function Wrapper(props) {
-  return (
 
-      <div className="container-fluid">
-      <Grid fluid={true}>
+function LeftColumn(props) {
+  return (
       <Col sm={9}>
         <Row>
 
@@ -20,32 +18,50 @@ function Wrapper(props) {
           {props.questions}
         </Row>
       </Col>
-      <Col sm={3}>
+  );
+}
+
+
+function RightColumn(props) {
+  return (
+  <Col sm={3}>
 
             {props.chat}
             {props.input}
             {props.login}
      </Col>
+     );
+}
+
+
+
+class Main extends React.Component {
+
+  state = { showing: true };
+
+  render(){
+    const { showing } = this.state;
+    return(
+
+    <div className="container-fluid">
+      <Grid fluid={true}>
+       <Button onClick={() => this.setState({ showing: !showing })}>toggle</Button>
+                { showing 
+      ? <LeftColumn stream={<StreamingScreen />}
+               questions={<QuestionSection />} />
+      : null 
+    }
+      <RightColumn input={<TextInput />}
+               login = {<Login/>}
+               chat={<ChatSection />} />
+
+
         </Grid>
       </div>
 
-    );
+        )
   }
 
-
-var Main = React.createClass({
-  render: function(){
-    return(
-      <Wrapper stream={<StreamingScreen />}
-               chat={<ChatSection />}
-               questions={<QuestionSection />}
-               input={<TextInput />}
-               login = {<Login/>}
-                />
-
-        );
-  }
-
-})
+}
 
 export default Main
